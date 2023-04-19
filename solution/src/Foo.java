c5c947e1e8d974d3d2ef0fa0fd6d08b2ea69f37d
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-04-19 15:07:53 +0200

    Finish work on Foo function

--- solution/src/Foo.java
+++ solution/src/Foo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.*
 
 public class Foo
 {
@@ -11,7 +12,46 @@ public class Foo
      *  2. List (STRING_ARRAY) - shoppingCart
      */
 
+    private boolean checkFruit(String codeFruit, String shoppingFruit){
+	return codeFruit.equals(shoppingFruit) || codeFruit.equals("anything") && !shoppingFruit.equals("nothing");
+    }
+
     public static int IsBuyerWinner(ArrayList<String> codeList, ArrayList<String> shoppingCart) {
-        return 0;
+		
+	Iterator<ArrayList<String>> codeItterator = codeList.iterator();
+	Iterator<String> shoppingCartItterator = shoppingCart.iterator();
+	
+	ArrayList<String> currentCodeList = codeItterator.next();
+	Iterator<String> currentCodeListIterator = currentCodeList.iterator();
+
+	String currentShoppingItem = shoppingCartItterator.next();
+	int offset = 0;
+
+	for (int i = shoppingCart.size(); i > 0; i--){
+		String curShopItem = currentShoppingItem.next();
+
+		if (currentCodeList.size() > i)
+			return 0;
+		else if checkFruit(currentCodeListIterator.next(), curShopItem) {
+			boolean patternFound = true;
+			while (currentCodeListIterator.hasNext()){
+				i--;
+				if !checkFruit(currentCodeListIterator.next(), currentShoppingItem.next()) {
+					patternFound = false;
+					currentCodeListIterator = currentCodeList.iterator();
+					break;
+				}
+			}
+
+			if (patternFound){
+				if (codeItterator.hasNext())
+					currentCodeList = codeItterator.next();
+				else
+					return 1;
+			}
+		}
+	}
+
+	return 0;
     }
 }
