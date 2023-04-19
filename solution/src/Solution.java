d400af161a52f3adfe6c439fa631e739afa9ff1f
VedadSiljic <siljicvedad17@gmail.com>
2023-04-19 18:01:22 +0200

    Rework the input

--- solution/src/Solution.java
+++ solution/src/Solution.java
@@ -1,41 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int codeListCount = scanner.nextInt();
         scanner.nextLine();
 
 
-        ArrayList<ArrayList<String>> codeList = new ArrayList<ArrayList<String>>();
+        String[][] codeList = new String[codeListCount][];
 
         for (int i = 0; i < codeListCount; i++) {
-            int listCount = scanner.nextInt();
-            scanner.nextLine();
-            ArrayList<String> list = new ArrayList<String>();
-            for (int j = 0; j < listCount; j++) {
-                String codeListItem = scanner.nextLine();
-                list.add(codeListItem);
-            }
-            codeList.add(list);
+            String codes = scanner.nextLine();
+            codeList[i] = codes.split(" ");
         }
 
-        int shoppingCartCount = scanner.nextInt();
-        scanner.nextLine();
-
-
-        ArrayList<String> shoppingCart = new ArrayList<String>();
-
-        for (int i = 0; i < shoppingCartCount; i++)
-        {
-            String shoppingCartItem = scanner.nextLine();
-            shoppingCart.add(shoppingCartItem);
-        }
+        String shoppingCartString = scanner.nextLine();
+        String[] shoppingCart = shoppingCartString.split(" ");
 
         int foo = Foo.IsBuyerWinner(codeList, shoppingCart);
-
         System.out.println(foo);
     }
 }
